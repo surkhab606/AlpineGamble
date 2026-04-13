b557dadd0a4ee51512f60ec6fd33ace03cc17001
Surkhab Mundi <surkhabm1@gmail.com>
2026-04-12 21:31:50 -0600

    Created test map of playerCards and making player gameplay loop

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream> 
+#include <string> 
+#include <map> 
 using namespace std;
 
 //PLAYER CHOOSES TO MOVE FORWARD, PLAY CARD OR STAY PUT
@@ -10,7 +12,44 @@ using namespace std;
 //PLAYER GOAL: GET TO SUMMIT AND COME DOWN
 //MOUNTAIN GOAL: KILL PLAYER
 
+map<string, string> playerCards() { 
+    map<string, string> cardList = {{"1) Granola Bar", "Restore 5% of your Total Energy."}, 
+                                    {"2) Red Talisman", "Your next move uses 150% less Total Energy. Discarded upon use."},
+                                    {"3) Bag of Trail Mix", "Restore 15% of your Total Energy."}};
+    
+    return cardList; 
+
+}
+
+
+void playerMove(int playerChoice) { 
+    if(playerChoice == 1) { 
+        cout << "You move forward.";
+    }
+
+    else if (playerChoice == 2) { 
+        int cardSelect; 
+        cout << "What card would you like to play? Select 0 to view available cards, or select 1, 2 or 3 to play that card." << "\n"; 
+        cin >> cardSelect; 
+        switch (cardSelect) {
+            case 0: 
+                for (auto card : playerCards)
+        }
+
+    }
+}
+
 int main() { 
+
+    //PLAYER always starts first 
+    int playerTurn = 1; 
+    cout << "PRESS (1) TO MOVE FORWARD. (2) TO PLAY A CARD. (3) TO STAY PUT." << "\n"; 
+    int userChoice; 
+    cin >> userChoice; 
+    playerMove(userChoice); 
+
+
+
 
     return 0;   
 
