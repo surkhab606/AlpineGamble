ada8ba279145c58b05bcfc130686a3f4db962c8f
30189028 <surkhab.mundi@ucalgary.ca>
2026-04-17 19:45:51 -0600

    Restarting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream> 
 #include <string> 
 #include <map> 
+#include <mountain.cpp> 
+
+
 using namespace std;
 
 //PLAYER CHOOSES TO MOVE FORWARD, PLAY CARD OR STAY PUT
@@ -14,73 +17,7 @@ using namespace std;
 
 
 
-map<string, string> mountainCards() { 
-    map<string, string> mountainCardList = {{"...", "..."},
-                                            {"...", "..."},
-                                            {"...", "..."}, 
-                                            {"...", "..."}};
-    
-    return mountainCardList; 
-}
-
-
-
-
-map<string, string> playerCards() { 
-    map<string, string> playerCardList = {{"1) Granola Bar", "Restore 5% of your Total Energy."}, 
-                                    {"2) Red Talisman", "Your next move uses 150% less Total Energy. Discarded upon use."},
-                                    {"3) Bag of Trail Mix", "Restore 15% of your Total Energy."}};
-    
-    return playerCardList; 
-
-}
-
-
-
-void mountainMove() { 
-    
-}
-
-
-
-
-
-
-
-
-
-
-void playerMove(int playerChoice) { 
-    if(playerChoice == 1) { 
-        cout << "You move forward.";
-    }
-
-    else if (playerChoice == 2) { 
-        int playingCardDecision; 
-        cout << "What card would you like to play? Select 0 to view available cards, or select 1, 2 or 3 to play that card." << "\n"; 
-        cin >> playingCardDecision; 
-        while (playingCardDecision == 0) { 
-            for (auto card : playerCards()) { 
-                cout << card.first << ": " << card.second << "\n\n"; 
-            }
-            cout << "What card would you like to play? Select 1, 2 or 3 to play that card." << "\n"; 
-            cin >> playingCardDecision; 
-
-        };
-
-    }
-}
-
 int main() { 
-
-    //PLAYER always starts first 
-    int playerTurn = 1; 
-    cout << "PRESS (1) TO MOVE FORWARD. (2) TO PLAY A CARD. (3) TO STAY PUT." << "\n"; 
-    int userChoice; 
-    cin >> userChoice; 
-    playerMove(userChoice); 
-    playerTurn = 0; 
-
 
 
 
---
+++ mountain.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <string>
+
+
+class Mountain {
+    //mountain name 
+    //mountain description
+    //mountain map
+    //mountain cards
+    //mountain AI
+
+}; 
